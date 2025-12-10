e6c3ebc131ddfd37a56f0e7b0dda2720b0fb85ef
Nermin Sehic <sehicnermin97@gmail.com>
2025-12-10 22:56:37 +1100

    Revert colorscheme

--- home/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ home/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,36 +1,19 @@
 return {
-  'webhooked/kanso.nvim',
+  'neanias/everforest-nvim',
+  version = false,
   lazy = false,
-  priority = 1000,
+  priority = 1000, -- make sure to load this before all the other start plugins
+  -- Optional; default configuration will be used if setup isn't called.
   config = function()
-    require('kanso').setup {
-      bold = false, -- enable bold fonts
-      italics = false, -- enable italics
-      compile = false, -- enable compiling the colorscheme
-      undercurl = true, -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true },
-      statementStyle = {},
-      typeStyle = {},
-      transparent = false, -- do not set background color
-      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true, -- define vim.g.terminal_color_{0,17}
-      colors = { -- add/modify theme and palette colors
-        palette = {},
-        theme = { zen = {}, pearl = {}, ink = {}, all = {} },
-      },
-      overrides = function(colors) -- add/modify highlights
-        return {}
-      end,
-      background = { -- map the value of 'background' option to a theme
-        dark = 'ink', -- try "zen", "mist" or "pearl" !
-        light = 'ink', -- try "zen", "mist" or "pearl" !
-      },
-      foreground = 'default', -- "default" or "saturated" (can also be a table like background)
+    require('everforest').setup {
+      background = 'hard',
+      transparent_background_level = 0,
+      italics = true,
+      disable_italic_comments = false,
+      ui_contrast = 'low',
+      dim_inactive_windows = false,
+      spell_foreground = false,
     }
-
-    -- setup must be called before loading
-    vim.cmd 'colorscheme kanso'
+    vim.cmd.colorscheme 'everforest'
   end,
 }
