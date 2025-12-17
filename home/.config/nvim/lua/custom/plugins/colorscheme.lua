faa4c4a2d9063fcb6a312d1f45857b2c971df7ba
Nermin Sehic <sehicnermin97@gmail.com>
2025-12-17 20:21:06 +1100

    Gruvbox

--- home/.config/nvim/lua/custom/plugins/colorscheme.lua
+++ home/.config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,19 +1,30 @@
 return {
-  'neanias/everforest-nvim',
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
+  'ellisonleao/gruvbox.nvim',
+  priority = 1000,
   config = function()
-    require('everforest').setup {
-      background = 'hard',
-      transparent_background_level = 0,
-      italics = true,
-      disable_italic_comments = false,
-      ui_contrast = 'low',
-      dim_inactive_windows = false,
-      spell_foreground = false,
+    require('gruvbox').setup {
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = 'hard', -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
     }
-    vim.cmd.colorscheme 'everforest'
+    vim.cmd 'colorscheme gruvbox'
   end,
 }
